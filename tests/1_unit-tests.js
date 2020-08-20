66cee1f7bae9f2b29e31cef84ed624155e5a894c
Dave Baskind <dave@picketstudio.com>
2020-08-21 08:52:10 +1000

    Challenge 1

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -10,7 +10,7 @@ suite('Unit Tests', function(){
   
     /** 1 - Use assert.isNull() or assert.isNotNull() to make the tests pass. **/
     test('#isNull, #isNotNull', function(){
-      assert.isNull(null, 'this is an optional error description - e.g. null is null');
+      assert.isNull(null, 'Null is, in fact, null');
       assert.isNotNull( 1, '1 is not null');
     });
   
